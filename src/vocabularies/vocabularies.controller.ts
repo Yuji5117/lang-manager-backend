8c0d69b6dfd24c88d47e7547633919e833cb5586
Yuji Nakamura <ny5117@icloud.com>
2022-01-23 19:46:20 +0900

    追加・更新・削除処理を実装

--- src/vocabularies/vocabularies.controller.ts
+++ src/vocabularies/vocabularies.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { VocabulariesService } from './vocabularies.service';
 import { Vocabulary } from './vocabularies.entity';
 
@@ -9,5 +17,22 @@ export class VocabulariesController {
   @Get()
   findAll(): Promise<Vocabulary[]> {
     return this.vocabulariesService.findAll();
+  }
+
+  @Post()
+  async add(
+    @Body() vocabulary: { word: string; translatedWord: string; image: string },
+  ): Promise<void> {
+    await this.vocabulariesService.add(vocabulary);
+  }
+
+  @Put(`:id`)
+  async update(@Param() id, @Body() vocabulary): Promise<void> {
+    await this.vocabulariesService.update(id, vocabulary);
+  }
+
+  @Delete(`:id`)
+  async delete(@Param() id): Promise<void> {
+    await this.vocabulariesService.delete(id);
   }
 }
--- src/vocabularies/vocabularies.service.ts
+++ src/vocabularies/vocabularies.service.ts
@@ -13,4 +13,16 @@ export class VocabulariesService {
   findAll(): Promise<Vocabulary[]> {
     return this.vocabulariesRepository.find();
   }
+
+  async add(params): Promise<void> {
+    await this.vocabulariesRepository.insert(params);
+  }
+
+  async update(id, vocabulary): Promise<void> {
+    await this.vocabulariesRepository.update(id, vocabulary);
+  }
+
+  async delete(id): Promise<void> {
+    await this.vocabulariesRepository.delete(id);
+  }
 }
